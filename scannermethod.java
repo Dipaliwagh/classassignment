07fef58c5843313e0ab805f3122ab7e1148da1da
Dipaliwagh <117341163+Dipaliwagh@users.noreply.github.com>
2022-11-03 12:43:42 +0530

    Add files via upload

---
+++ scannermethod.java
@@ -0,0 +1,22 @@
+
+public class scannermethod {
+	package javapractical;
+
+	import java.util.Scanner;
+
+	public class HelloWordprogram {
+		public static void main(String []args)
+		{
+			Scanner scan=new Scanner(System.in);
+			int num;
+			System.out.println("enter one number");
+			
+			num=scan.nextInt();//20
+			System.out.println("the value you have entered: "+ num);
+			
+		}
+		
+		
+
+	}
+}
